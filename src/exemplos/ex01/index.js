93f6a7e08ca65990f768f434efd6abc27de1e40e
Joaogabriel6 <jgabriel2905@gmail.com>
2025-08-15 22:46:45 -0300

    Começando Exemplo01

---
+++ src/exemplos/ex01/index.js
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+
+export default function Exemplo01() {
+    return{
+        <View>
+            <Text> Exemplo01 </Text>;
+            <Text> Introdução React Native com Expo </Text>
+        </View>
+    };
+}
